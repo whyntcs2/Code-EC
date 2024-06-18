13768fde8ff261ec4283b028772f0e20bcace3d1
whyntcs2 <168003164+whyntcs2@users.noreply.github.com>
2024-06-18 19:43:35 +0700

    Update B27.cpp

--- B27.cpp
+++ B27.cpp
@@ -1,14 +1,13 @@
 
 #include<stdio.h>
 #include<string.h>
-int main()
-{
+int main(){
 	char a[100];
 	gets(a);
 	int s=0;
 	for(int i=0;i<strlen(a);i++)
 	{
-		s=s+1;
-		}
-		printf("%d", s);
-		}
+	    s=s+1;
+	}
+	printf("%d", s);
+}
